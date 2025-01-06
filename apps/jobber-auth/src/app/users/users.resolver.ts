f1e894c6262fc7985eba1a83056d28fdb970deb2
kozlowskimateusz <mateusz.kozlowski@nextapps.co>
2025-01-06 18:46:17 +0100

    Added jwt auth guard

--- apps/jobber-auth/src/app/users/users.resolver.ts
+++ apps/jobber-auth/src/app/users/users.resolver.ts
@@ -2,6 +2,10 @@ import { UsersService } from './users.service';
 import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { User } from './models/user.model';
 import { CreateUserInput } from './dto/create-user.input';
+import { UseGuards } from '@nestjs/common';
+import { GqlAuthGuard } from '../auth/guards/gql-auth.guard';
+import { CurrentUser } from '../auth/decorators/current-user.decorator';
+import { TokenPayload } from '../auth/interface/token-payload.interface';
 
 @Resolver(() => User)
 export class UsersResolver {
@@ -13,7 +17,10 @@ export class UsersResolver {
   }
 
   @Query(() => [User], { name: 'users' })
-  async getUsers() {
+  @UseGuards(GqlAuthGuard)
+  async getUsers(@CurrentUser() user: TokenPayload) {
+    console.log(user);
+
     return this.usersService.getUsers();
   }
 }
