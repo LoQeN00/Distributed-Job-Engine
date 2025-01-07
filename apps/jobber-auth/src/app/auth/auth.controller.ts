ebbe50c0ba331328da239d98901f80ccbfd8df88
kozlowskimateusz <mateusz.kozlowski@nextapps.co>
2025-01-07 22:01:10 +0100

    Guard jobber-job microservice using jobber-auth microservice

---
+++ apps/jobber-auth/src/app/auth/auth.controller.ts
@@ -0,0 +1,26 @@
+import { UsersService } from './../users/users.service';
+import { Controller, UseGuards } from '@nestjs/common';
+import { Observable } from 'rxjs';
+import {
+  AuthenticateRequest,
+  AuthServiceController,
+  AuthServiceControllerMethods,
+  User,
+} from 'types/proto/auth';
+import { JwtAuthGuard } from './guards/jwt-auth.guard';
+import { TokenPayload } from './interface/token-payload.interface';
+
+@Controller()
+@AuthServiceControllerMethods()
+export class AuthController implements AuthServiceController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @UseGuards(JwtAuthGuard)
+  authenticate(
+    request: AuthenticateRequest & { user: TokenPayload }
+  ): Promise<User> | Observable<User> | User {
+    console.log(request);
+
+    return this.usersService.getUserOrThrow({ id: request.user.userId });
+  }
+}
--- apps/jobber-jobs/src/app/jobs/jobs.module.ts
+++ apps/jobber-jobs/src/app/jobs/jobs.module.ts
@@ -1,11 +1,26 @@
+import { AUTH_PACKAGE_NAME } from 'types/proto/auth';
 import { Module } from '@nestjs/common';
 import { DiscoveryModule } from '@golevelup/nestjs-discovery';
 import { FibbonaciJob } from './job-types/fibbonaci.job';
 import { JobsService } from './jobs.service';
 import { JobsResolver } from './jobs.resolver';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { join } from 'path';
 
 @Module({
-  imports: [DiscoveryModule],
+  imports: [
+    DiscoveryModule,
+    ClientsModule.register([
+      {
+        name: AUTH_PACKAGE_NAME,
+        transport: Transport.GRPC,
+        options: {
+          package: AUTH_PACKAGE_NAME,
+          protoPath: join(__dirname, 'proto/auth.proto'),
+        },
+      },
+    ]),
+  ],
   providers: [FibbonaciJob, JobsService, JobsResolver],
 })
 export class JobsModule {}
